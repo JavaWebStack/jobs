11e454a919bc79ce856e3328a80e9881a36846bc
x7airworker <jake@jakes-co.de>
2023-02-20 16:17:43 +0100

    Rebuild to use type instead of job_id

--- src/main/java/org/javawebstack/jobs/storage/model/RecurringJobInfo.java
+++ src/main/java/org/javawebstack/jobs/storage/model/RecurringJobInfo.java
@@ -11,27 +11,31 @@ import java.util.UUID;
 @Setter
 public class RecurringJobInfo {
 
-    UUID jobId;
+    UUID id;
+    String type;
     CronInterval cron;
     Date lastExecutionAt;
     Date createdAt;
 
     public RecurringJobInfo clone() {
         return new RecurringJobInfo()
-                .setJobId(jobId)
+                .setId(id)
+                .setType(type)
                 .setCron(cron)
                 .setLastExecutionAt(lastExecutionAt)
                 .setCreatedAt(createdAt);
     }
 
     public void checkRequired() throws IllegalArgumentException {
-        if (jobId == null)
-            throw new IllegalArgumentException("Recurring Job jobId is required");
+        if (type == null)
+            throw new IllegalArgumentException("Recurring Job type is required");
         if (cron == null)
             throw new IllegalArgumentException("Recurring Job cron is required");
     }
 
     public void sanitize() {
+        if (id == null)
+            id = UUID.randomUUID();
         if (createdAt == null)
             createdAt = new Date();
         if (lastExecutionAt == null)
