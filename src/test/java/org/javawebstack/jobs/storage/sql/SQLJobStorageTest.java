f29ff8870677aac7400a3f0300b17cd02d53d13a
JanHolger <jan@bebendorf.eu>
2023-09-20 22:12:57 +0200

    Fixed failed test

--- src/test/java/org/javawebstack/jobs/storage/sql/SQLJobStorageTest.java
+++ src/test/java/org/javawebstack/jobs/storage/sql/SQLJobStorageTest.java
@@ -2,29 +2,27 @@ package org.javawebstack.jobs.storage.sql;
 
 import org.javawebstack.jobs.storage.JobStorage;
 import org.javawebstack.jobs.storage.JobStorageTest;
-import org.javawebstack.jobs.storage.sql.SQLJobStorage;
 import org.javawebstack.jobs.test.TestProperties;
 import org.javawebstack.jobs.test.TestUtil;
 import org.javawebstack.jobs.test.precondition.SQLDatabaseAvailable;
-import org.javawebstack.orm.wrapper.SQL;
-import org.junit.jupiter.api.AfterAll;
+import org.javawebstack.orm.connection.pool.SQLPool;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(SQLDatabaseAvailable.class)
 public class SQLJobStorageTest extends JobStorageTest {
 
-    private SQL sql;
+    private SQLPool pool;
 
     protected JobStorage initStorage() {
-        sql = TestProperties.createSQLDatabaseConnection();
-        TestUtil.purgeStorageDatabase(sql);
-        return new SQLJobStorage(sql, null);
+        pool = TestProperties.createSQLDatabaseConnection();
+        TestUtil.purgeStorageDatabase(pool);
+        return new SQLJobStorage(pool, null);
     }
 
     @AfterEach
     public void cleanup() {
-        TestUtil.purgeStorageDatabase(sql);
+        TestUtil.purgeStorageDatabase(pool);
     }
 
 }
